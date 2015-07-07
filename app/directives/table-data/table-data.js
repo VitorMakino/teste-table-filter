b36917aea718c853a53a870f97b6363620b65d00
VitorMakino <vitor.makino@gmail.com>
2015-07-07 01:03:25 -0300

    Added support for date filter

--- app/directives/table-data/table-data.js
+++ app/directives/table-data/table-data.js
@@ -1,38 +1,80 @@
-   angular.module('searchTable',['ui.bootstrap'])
+
+var Operator = function() {
+  this.operation = "";
+};
+ 
+Operator.prototype = {
+    setStrategy: function(operator) {
+		this.operation = operator;
+    },
+ 
+    operate: function(value,type,search) {
+        return this.operation.operate(value,type,search);
+    }
+};
+
+var EqualsOperator = function() {
+    this.name = 'equals';
+    this.operate = function(value,type,search) {
+        return value && search && value == search;
+    }
+};
+
+var LikeOperator = function() {
+    this.name = 'like';
+    this.operate = function(value,type,search) {
+        return (""+value).indexOf(search) > -1;
+    }
+};
+
+var GreaterThanOperator = function() {
+    this.name = 'greater than';
+    this.operate = function(value,type,search) {
+        return value > search;
+    }
+};
+
+var LessThanOperator = function() {
+    this.name = 'less than';
+    this.operate = function(value,type,search) {
+        return value < search;
+    }
+};
+ 
+ angular.module('searchTable',['ui.bootstrap'])
      .directive('tableFilter',['$location',function() {
        return {
          templateUrl:'directives/table-data/table-filter.html',
          restrict: 'E',
-         //replace: true,
-   	  controller : function($scope) {
-   	    $scope.filters = [{}];
-   		$scope.newFilter = {};
-   		$scope.operators = ['equals','like','greater than','less than'];
-
-    	$scope.addFilter = function() {
-		    if($scope.isValid()) {
-    		    $scope.filters.push($scope.newFilter);
-    			$scope.newFilter = {};
-			}
-   		  }
-   		  
-   		  $scope.showRemoveButton = function() {
-   		    return $scope.filters.length > 1;
-   		  }
-   		  
-   		  $scope.removeFilter = function(index) {
-   			$scope.filters.splice(index,1);
-   		  }
-   		  
-   		  $scope.isValid = function() {
-   		    var valid =  $scope.searchForm.$valid;
-   		    return valid;
-   		  }
-         },
-   	  scope: {
-           columns: '=columns',
-   		   filters: '=query'
-         }
+		 controller : function($scope) {
+		    $scope.filters = [{}];
+		    $scope.newFilter = {};
+		    $scope.operators = [ new EqualsOperator(),new LikeOperator(),new GreaterThanOperator(),new LessThanOperator()];
+            
+		    $scope.addFilter = function() {
+		    	if($scope.isValid()) {
+		    		$scope.filters.push($scope.newFilter);
+		    		$scope.newFilter = {};
+		    	}
+		      }
+		      
+		      $scope.showRemoveButton = function() {
+		    	return $scope.filters.length > 1;
+		      }
+		      
+		      $scope.removeFilter = function(index) {
+		    	$scope.filters.splice(index,1);
+		      }
+		      
+		      $scope.isValid = function() {
+		    	var valid =  $scope.searchForm.$valid;
+		    	return valid;
+		      }
+		  },
+		   scope: {
+		 	   columns: '=columns',
+		 	   filters: '=query'
+		 	 }
        }
      }])
     .filter('queryFilter', function() {
@@ -40,20 +82,22 @@
    
        var output = [];
 	   var operatorStrategy = new Operator();
-	   this.operate = function(value,operator,searchValue) {
+	   this.operate = function(value,operator,type,searchValue) {
 	     operatorStrategy.setStrategy(operator);
-		 return operatorStrategy.operate(value,searchValue);
+		 return operatorStrategy.operate(value,type,searchValue);
 	   }
 	   
        angular.forEach(input, function(row) {
 	     output.push(row);
 		 for(i = 0; i < query.length; i++) { 
 		   var filtro = query[i];	 
-			if(angular.isDefined(filtro.name) &&
+			if(angular.isDefined(filtro.column) &&
+		       angular.isDefined(filtro.column.property) &&
                angular.isDefined(filtro.operator) &&
                angular.isDefined(filtro.value) &&
-			   !operate(row[filtro.name]
+			   !operate(row[filtro.column.property]
 			           ,filtro.operator
+					   ,filtro.column.type
 					   ,filtro.value)) {
 			    output.pop(row);
 				break;
@@ -69,7 +113,6 @@
     return {
       templateUrl:'directives/table-data/table-data.html',
       restrict: 'E',
-      //replace: true,
 	  controller : function($scope) {	  
 		  $scope.setOrderBy = function(column, sortReverse) {
 		   $scope.sortType = column.property;
@@ -105,51 +148,3 @@
     }
   }])
   ;
-
-
-  
-var Operator = function() {
-  this.operation = "";
-};
- 
-Operator.prototype = {
-    setStrategy: function(operator) {
-	    if(operator == 'equals') {
-		  this.operation = new EqualsOperator();
-		} else if(operator == 'like') {
-		  this.operation = new LikeOperator();
-		} else if(operator == 'greater than') {
-		  this.operation = new GreaterThanOperator();
-		} else if(operator == 'less than') {
-		  this.operation = new LessThanOperator();
-		}
-    },
- 
-    operate: function(value,search) {
-        return this.operation.operate(value,search);
-    }
-};
-
-var EqualsOperator = function() {
-    this.operate = function(value,search) {
-        return (""+value) == (""+search);
-    }
-};
-
-var LikeOperator = function() {
-    this.operate = function(value,search) {
-        return (""+value).indexOf(search) > -1;
-    }
-};
-
-var GreaterThanOperator = function() {
-    this.operate = function(value,search) {
-        return value > search;
-    }
-};
-
-var LessThanOperator = function() {
-    this.operate = function(value,search) {
-        return value < search;
-    }
-};
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,17 @@
 			  },{
 				name: "Price",
 				property:'price'
-			  }];									
+			  },
+			  {
+				name: "Expiration Date",
+				property:'expdt',
+				type: 'date'
+			  }
+			  ];									
 		  $scope.sushi = [
-			{ name: 'Cali Roll', fish: 'Crab', tastiness: 2, price: 1 },
-			{ name: 'Philly', fish: 'Tuna', tastiness: 4, price: 2 },
-			{ name: 'Tiger', fish: 'Eel', tastiness: 7, price: 3 },
-			{ name: 'Rainbow', fish: 'Variety', tastiness: 6, price: 4 }
+			{ name: 'Cali Roll', fish: 'Crab', tastiness: 2, price: 1, expdt: new Date(2015, 7, 7)  },
+			{ name: 'Philly', fish: 'Tuna', tastiness: 4, price: 2, expdt: new Date(2015, 7, 12)  },
+			{ name: 'Tiger', fish: 'Eel', tastiness: 7, price: 3, expdt: new Date(2015, 7, 15) },
+			{ name: 'Rainbow', fish: 'Variety', tastiness: 6, price: 4, expdt: new Date() }
 		  ]; 
 	} ]);
